2dfd2a0d97bb203791bc4b85ef923ec00daed084
tsizo <ee180485@n-fukushi.ac.jp>
2021-03-09 19:17:51 +0900

    メモ投稿機能サーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,11 @@ class PostsController < ApplicationController
   end
  
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
   def checked
+    binding.pry
     post = Post.find(params[:id])
     if post.checked 
       post.update(checked: false)
